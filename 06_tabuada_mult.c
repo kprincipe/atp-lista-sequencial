692dd1e297733f4a5439f5258e42501b08ac1069
kprincipe <kinanprincipe@outlook.com>
2025-08-21 16:07:34 -0400

    implementado algoritmo de tabuada

--- 06_tabuada_mult.c
+++ 06_tabuada_mult.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
+    short int valor = 0;
+
+    printf("Insira numero de 1 a 10: ");
+    if (scanf("%d", &valor) != 1) {
+        fprintf(stderr, "Erro: entrada deve ser numerica\n");
+        return 1;
+    }
+
+    if (valor > 10 || valor < 1) {
+        fprintf(stderr, "Erro: entrada deve ser numero de 1 a 9\n");
+        return 1;
+    }
     
+    for (int i = 1; i <= 10; ++i) {
+        printf("%d x %d = %d\n", i, valor, i * valor);
+    }
+    return 0;
 }
