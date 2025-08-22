3d7046a141fadfaa15f282838dc8c1eeebccade5
kprincipe <kinanprincipe@outlook.com>
2025-08-22 18:23:48 -0400

    correcao no numero de nomes em 17_imprimir_nomes

--- 17_imprimir_nomes.c
+++ 17_imprimir_nomes.c
@@ -3,6 +3,7 @@
 int main(void) {
     char nome1[256];
     char nome2[256];
+    char nome3[256];
 
     printf("Escreva o primeiro nome: ");
     fgets(nome1, 256, stdin);
@@ -10,8 +11,12 @@ int main(void) {
     printf("Escreva o segundo nome : ");
     fgets(nome2, 256, stdin);
 
+    printf("Escreva o terceiro nome : ");
+    fgets(nome3, 256, stdin);
+
     printf("\n");
     printf(": %s", nome1);
     printf(": %s", nome2);
+    printf(": %s", nome3);
     return 0;
 }
