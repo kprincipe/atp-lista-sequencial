a08925e08beb176e89be9763cdb669a2069d3858
kprincipe <kinanprincipe@outlook.com>
2025-08-25 13:05:29 -0400

    implementacao algoritmo que calcula idade e projeta ela em 2028

---
+++ 33_idade_projecao.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int receber_numero(int *valor) {
+    if (scanf("%d", valor) != 1) {
+        fprintf(stderr, "Erro: entrada deve ser numerica\n");
+        exit(1);
+    }
+    return *valor;
+}
+
+int main(void) {
+    int dia, mes, ano;
+    
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);
+    
+    long int ano_agora = tm->tm_year + 1900;
+    
+    printf("Digite o dia de nascimento: ");
+    if (receber_numero(&dia)  > 31) {
+        fprintf(stderr, "Erro: dia nao pode ser maior que 31\n");
+        return 1;
+    }
+    
+    printf("Digite o mes de nascimento: ");
+    if (receber_numero(&mes) > 12) {
+        fprintf(stderr, "Erro: mes nao pode ser maior que 12");
+        return 1;
+    }
+
+    printf("Digite o ano de nascimento (Ex. 2002): ");
+    receber_numero(&ano);
+
+    int idade = ano_agora - ano;
+
+    printf("\n");
+    printf("Sua idade: %d\n", idade);
+    printf("Em 2028 voce tera: %d anos\n", idade + (2028 - (int)ano_agora));
+    
+    return 0;
+}
