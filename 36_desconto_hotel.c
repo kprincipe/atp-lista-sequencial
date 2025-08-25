5eb528f4d276bd7f76c8aa2e8a5621fbf82cc5d2
kprincipe <kinanprincipe@outlook.com>
2025-08-25 15:23:55 -0400

    implementacao algoritmo calculo arredacao hotel

---
+++ 36_desconto_hotel.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+    unsigned int apartamentos;
+    float valor_diaria, arrecadacao, valor_promocional = 0;
+
+    printf("Apartamentos disponiveis: ");
+    scanf("%d", &apartamentos);
+
+    printf("Valor da diaria: ");
+    scanf("%f", &valor_diaria);
+
+    valor_promocional = valor_diaria * 0.75;
+
+    arrecadacao = valor_promocional * apartamentos;
+    
+    printf("\n");
+    printf("--------------------------------------------------\n");
+    printf("Valor promocional........................ %.2f\n", valor_promocional);
+    printf("Arrecadacao com ocupacao total:.......... %.2f\n", arrecadacao);
+    printf("Arrecadacao com 70%% ocupado:............. %.2f\n", valor_promocional * (apartamentos * 0.7));
+    printf("Valor nao arrecadado com ocupacao total:. %.2f\n", (valor_diaria * apartamentos) - arrecadacao);
+    printf("--------------------------------------------------\n");
+    return 0;
+}
