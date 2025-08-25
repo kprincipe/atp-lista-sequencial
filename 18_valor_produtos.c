8908c95b5a0918def1c4e12ba19b755bdb6488f1
kprincipe <kinanprincipe@outlook.com>
2025-08-25 09:55:43 -0400

    implementacao algoritmo de calculo do valor total de produtos

---
+++ 18_valor_produtos.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int receber_numero(float *valor) {
+    if (scanf("%f", valor) != 1) {
+        fprintf(stderr, "Erro: entrada deve ser numerica\n");
+        exit(1);
+    }
+    return *valor;
+}
+
+int main(void) {
+    float quantidade1, valor1;
+    float quantidade2, valor2;
+    float quantidade3, valor3;
+    float valor_total = 0;
+
+    printf("Digite a quantidade do produto 1: ");
+    receber_numero(&quantidade1);
+    printf("    Valor: ");
+    receber_numero(&valor1);
+
+    printf("Digite a quantidade do produto 2: ");
+    receber_numero(&quantidade2);
+    printf("    Valor: ");
+    receber_numero(&valor2);
+
+    printf("Digite a quantidade do produto 3: ");
+    receber_numero(&quantidade3);
+    printf("    Valor: ");
+    receber_numero(&valor3);
+
+    valor_total = (quantidade1 * valor1) + (quantidade2 * valor2) + (quantidade3 * valor3);
+
+    printf("\n");
+    printf("Valor total: %.2f\n", valor_total);
+    
+    return 0;
+}
