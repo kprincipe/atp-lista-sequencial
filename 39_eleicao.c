0edcde7e7c47da0abf19ae5dceb5fc552395de65
kprincipe <kinanprincipe@outlook.com>
2025-08-26 20:25:46 -0400

    correcao de sequenciamento

--- 39_eleicao.c
+++ 39_eleicao.c
@@ -3,7 +3,8 @@
 int main(void) {
     // declaracao de variaveis
     float total_eleitores;
-    float votos_cand1, votos_cand2;
+    float votos_cand1, votos_cand2, percentual_nulos, total_votos;
+    float percentual_cand1, percentual_cand2;
 
     // entrada de dados
     printf("Digite o numero total de eleitores: ");
@@ -16,10 +17,18 @@ int main(void) {
     scanf("%f", &votos_cand2);
 
     // processamento de dados inseridos
+    total_votos = votos_cand1 + votos_cand2;
+    
+    percentual_cand1 = (votos_cand1 / total_eleitores) * 100;
+    percentual_cand2 = (votos_cand2 / total_eleitores) * 100;
+
+    percentual_nulos = 100 - (percentual_cand1 + percentual_cand2);
+
+    // saida de dados processados
     printf("--------------------------------\n");
-    printf("Percentual candidato 1: %.2f%%\n", (votos_cand1 / total_eleitores) * 100);
-    printf("Percentual candidato 2: %.2f%%\n", (votos_cand2 / total_eleitores) * 100);
-    printf("Total de votos nulos  : %.2f\n", total_eleitores - (votos_cand1 + votos_cand2));
+    printf("Percentual candidato 1: %.2f%%\n", percentual_cand1);
+    printf("Percentual candidato 2: %.2f%%\n", percentual_cand2);
+    printf("Total de votos nulos  : %.2f%%\n", percentual_nulos);
     printf("--------------------------------\n");
 
     // saida de dados processados
